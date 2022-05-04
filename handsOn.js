e612f1747d3b177d0ad4aec1fe9d06b1279e8781
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 12:34:34 +0530

    Understanding Undefined Value returned from a Function

--- handsOn.js
+++ handsOn.js
@@ -1,11 +1,18 @@
 // Setup
-const outerWear = "T-Shirt";
+let sum = 0;
 
-function myOutfit() {
-  // Only change code below this line
-  let outerWear = "sweater";
-  // Only change code above this line
-  return outerWear;
+function addThree() {
+  sum = sum + 3;
 }
 
-myOutfit();
+// Only change code below this line
+function addFive(){
+  sum+=5;
+  let t;
+  return t;
+}
+
+// Only change code above this line
+
+addThree();
+addFive();
