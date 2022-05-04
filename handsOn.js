faa5171a04cc398eb8b7d8f3813570a6c9650348
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 16:42:39 +0530

    Accessing Nested Objects

--- handsOn.js
+++ handsOn.js
@@ -1,16 +1,13 @@
-const myMusic = [
-    {
-      artist: "Billy Joel",
-      title: "Piano Man",
-      release_year: 1973,
-      formats: ["CD", "8T", "LP"],
-      gold: true
-    },
-    // Add record here
-    {
-      artist: "Deep Purple",
-      title: "Smoke on the water",
-      release_year: 1976,
-      formats: ["CD", "8T", "LP"]
+const myStorage = {
+    "car": {
+      "inside": {
+        "glove box": "maps",
+        "passenger seat": "crumbs"
+       },
+      "outside": {
+        "trunk": "jack"
+      }
     }
-  ];
+  };
+  
+  const gloveBoxContents = myStorage.car.inside["glove box"];
