9cd5a4ff1ac9d80a7fa7074f9eb14641127c3eb8
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 17:18:09 +0530

    Profile Lookup

--- handsOn.js
+++ handsOn.js
@@ -1,6 +1,42 @@
-function sum(arr, n) {
-    // Only change code below this line
-    if(n<1)  return 0;
-    else return arr[n-1]+sum(arr,n-1);
-    // Only change code above this line
+// Setup
+const contacts = [
+    {
+      firstName: "Akira",
+      lastName: "Laine",
+      number: "0543236543",
+      likes: ["Pizza", "Coding", "Brownie Points"],
+    },
+    {
+      firstName: "Harry",
+      lastName: "Potter",
+      number: "0994372684",
+      likes: ["Hogwarts", "Magic", "Hagrid"],
+    },
+    {
+      firstName: "Sherlock",
+      lastName: "Holmes",
+      number: "0487345643",
+      likes: ["Intriguing Cases", "Violin"],
+    },
+    {
+      firstName: "Kristian",
+      lastName: "Vos",
+      number: "unknown",
+      likes: ["JavaScript", "Gaming", "Foxes"],
+    },
+  ];
+  
+  function lookUpProfile(name, prop) {
+    for (let x = 0; x < contacts.length; x++) {
+      if (contacts[x].firstName === name) {
+        if (contacts[x].hasOwnProperty(prop)) {
+          return contacts[x][prop];
+        } else {
+          return "No such property";
+        }
+      }
+    }
+    return "No such contact";
   }
+  
+  lookUpProfile("Akira", "likes");
