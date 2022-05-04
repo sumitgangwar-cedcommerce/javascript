af89fe10653057452655f3adb16438f8da61d335
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 17:05:04 +0530

    Nesting For Loops

--- handsOn.js
+++ handsOn.js
@@ -1,8 +1,13 @@
-// Setup
-const myArr = [2, 3, 4, 5, 6];
-
-// Only change code below this line
-var total = 0;
-for(var i=0;i<myArr.length;i++){
-  total+=myArr[i];
-}
+function multiplyAll(arr) {
+    let product = 1;
+    // Only change code below this line
+  for(var i=0;i<arr.length;i++){
+    for(var j=0;j<arr[i].length;j++){
+      product*=arr[i][j];
+    }
+  }
+    // Only change code above this line
+    return product;
+  }
+  
+  multiplyAll([[1, 2], [3, 4], [5, 6, 7]]);
