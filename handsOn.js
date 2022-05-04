0822685d3bca05f9e19ef4f39df719f98f61ef2e
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 11:39:24 +0530

    Appending Variables to Strings

--- handsOn.js
+++ handsOn.js
@@ -1,3 +1,4 @@
-// Only change code below this line
-const myName = "Sumit";
-const myStr = 'My Name is '+myName+' and I am wall!';
+// Change code below this line
+const someAdjective = "Sum";
+let myStr = "Learning to code is ";
+myStr+=someAdjective;
