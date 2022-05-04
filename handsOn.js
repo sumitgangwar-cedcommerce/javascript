8088df1ec90d2c35c36606415b7438a9e2c9bcb6
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 13:30:04 +0530

    Comparisons with the Logical Or Operator

--- handsOn.js
+++ handsOn.js
@@ -1,14 +1,12 @@
-function testLogicalAnd(val) {
+function testLogicalOr(val) {
     // Only change code below this line
   
-    if (val<=50 && val>=25) {
-       
-        return "Yes";
-      
+    if (val<10 || val>20) {
+      return "Outside";
     }
   
     // Only change code above this line
-    return "No";
+    return "Inside";
   }
   
-  testLogicalAnd(10);
+  testLogicalOr(15);
