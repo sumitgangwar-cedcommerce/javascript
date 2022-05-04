afd4d2ec354ff6526b2079372022827f376a7b27
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 16:50:59 +0530

    Record Collection

--- handsOn.js
+++ handsOn.js
@@ -1,20 +1,36 @@
-const myPlants = [
-    {
-      type: "flowers",
-      list: [
-        "rose",
-        "tulip",
-        "dandelion"
-      ]
+// Setup
+var recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
     },
-    {
-      type: "trees",
-      list: [
-        "fir",
-        "pine",
-        "birch"
-      ]
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
     }
-  ];
+  };
   
-  const secondTree = myPlants[1].list[1];
+  // Only change code below this line
+  function updateRecords(records, id, prop, value) {
+    if (prop !== 'tracks' && value !== "") {
+      records[id][prop] = value;
+    } else if (prop === "tracks" && records[id].hasOwnProperty("tracks") === false) {
+      records[id][prop] = [value];
+    } else if (prop === "tracks" && value !== "") {
+      records[id][prop].push(value);
+    } else if (value === "") {
+      delete records[id][prop];
+    }
+    return records;
+  }
+  
+  updateRecords(recordCollection, 5439, 'artist', 'ABBA');
