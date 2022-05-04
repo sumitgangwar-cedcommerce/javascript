1c1f2d63b07f9468bb534157901f6cb55c2312e2
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 13:39:29 +0530

    Introducing Else If Statements

--- handsOn.js
+++ handsOn.js
@@ -1,17 +1,16 @@
-function testElse(val) {
-    let result = "";
-    // Only change code below this line
-  
-    if (val > 5) {
-      result = "Bigger than 5";
+function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
     }
-  
     else{
-      result = "5 or Smaller";
+      if (val < 5) {
+        return "Smaller than 5";
+      }
+      else {
+        return "Between 5 and 10";
+      }
     }
-  
-    // Only change code above this line
-    return result;
   }
   
-  testElse(4);
+  
+  testElseIf(7);
