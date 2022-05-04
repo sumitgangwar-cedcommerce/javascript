0a6001adf2a3e8dc4cdbab8dad4cb1c7228f8266
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 16:17:09 +0530

    Add New Properties to a JavaScript Object

--- handsOn.js
+++ handsOn.js
@@ -1,10 +1,9 @@
-// Setup
 const myDog = {
-    "name": "Coder",
+    "name": "Happy Coder",
     "legs": 4,
     "tails": 1,
     "friends": ["freeCodeCamp Campers"]
   };
+  myDog.bark = "bow-wow";
   
-  // Only change code below this line
-  myDog.name = "Happy Coder";
+  
