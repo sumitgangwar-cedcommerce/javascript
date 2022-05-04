3374794065d436e8abb0def19837b1d694fe9ae0
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 17:42:16 +0530

    Use Recursion to Create a Countdown

--- handsOn.js
+++ handsOn.js
@@ -1,5 +1,12 @@
-function checkSign(num) {
-    return num>0 ? "positive": num<0 ? "negative" : "zero"
-  }
+// Only change code below this line
+function countdown(n){
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1);
+      arr.unshift(n);
+      return arr;
+    }
   
-  checkSign(10);
+  }
+  // Only change code above this line
