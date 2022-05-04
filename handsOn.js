22deeeded1696dbf1b9daccb81f60b8b406ff84c
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-04 10:37:09 +0530

    Explore Differences Between the var and let Keywords

--- handsOn.js
+++ handsOn.js
@@ -1,9 +1,2 @@
-// Variable declarations
-var studlyCapVar;
-var properCamelCase = "A String";
-var titleCaseOver = 9000;
-
-// Variable assignments
-studlyCapVar = 10;
-properCamelCase = "A String";
-titleCaseOver = 9000;
+let catName = "Oliver";
+let catSound = "Meow!";
