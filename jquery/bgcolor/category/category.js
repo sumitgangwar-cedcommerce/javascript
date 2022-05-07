33bc9764a4d70e7f796e01418e51217627799caa
sumitgangwar-cedcommerce <sumitgangwar@cedcoss.com>
2022-05-07 11:53:23 +0530

    Category and sub-category

---
+++ jquery/bgcolor/category/category.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#clothes").click(function(){
+        $("#clot").css("display","block");
+    });
+    $("#t-shirt").click(function(){
+        $("#t-sh").css("display","block");
+    });
+    $("#trouser").click(function(){
+        $("#trou").css("display","block");
+    });
+    $("#electronics").click(function(){
+        console.log("jbgsdcv");
+        $("#elect").css("display","block");
+    });
+    $("#mobile").click(function(){
+        console.log("jbgsdcv");
+        $("#mob").css("display","block");
+    });
+    $("#tabs").click(function(){
+        console.log("jbgsdcv");
+        $("#ta").css("display","block");
+    });
+});
